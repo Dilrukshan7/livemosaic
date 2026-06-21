c6a08543a29c539ccb70970434e7ba7f84fcbd9b
Dilrukshan7 <dilrukshanofficial@gmail.com>
2026-06-21 17:06:26 +0530

    bug fixes #004 / front End URL fix

--- frontend/src/app/api/auth/callback/route.ts
+++ frontend/src/app/api/auth/callback/route.ts
@@ -1,13 +1,16 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { type EmailOtpType } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
+  const token_hash = url.searchParams.get("token_hash");
+  const type = url.searchParams.get("type") as EmailOtpType | null;
   const next = url.searchParams.get("next") ?? "/dashboard";
 
-  if (code) {
+  if (code || (token_hash && type)) {
     const cookieStore = await cookies();
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -22,7 +25,12 @@ export async function GET(request: Request) {
         },
       }
     );
-    await supabase.auth.exchangeCodeForSession(code);
+
+    if (code) {
+      await supabase.auth.exchangeCodeForSession(code);
+    } else if (token_hash && type) {
+      await supabase.auth.verifyOtp({ token_hash, type });
+    }
   }
 
   return NextResponse.redirect(new URL(next, url.origin));
